55ccedc6302fab0458306a4d642c230138c2eec9
Alexey Sukhoviy <martinsilenn@gmail.com>
2011-05-15 00:03:41 +0300

    Color marking for staffing calendar

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,19 @@
 module ProjectsHelper
+  def color_marking(this_bookings,date)
+    if date.saturday? or date.sunday?
+      marking = "weekend"
+    else
+      marking = "green_bg"
+    end
+    if this_bookings and this_bookings[@date]
+      if this_bookings[@date][1] > 0 and this_bookings[@date][1] < 8
+        marking = "yellow_bg"
+      else this_bookings[@date][1] >= 8
+        marking = "red_bg"
+      end
+    end
+    marking
+  end
+
 end
+
