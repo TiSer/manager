78699f90d91896038725bc3290a07f62a0b658fc
Alexey Sukhoviy <martinsilenn@gmail.com>
2011-06-21 19:03:12 +0300

    Milestones Expence 70%

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,5 +1,5 @@
 class Milestone < ActiveRecord::Base
-  
+
   belongs_to :project
 
   def by_time_amount(mile)
@@ -14,4 +14,25 @@ class Milestone < ActiveRecord::Base
     summ
   end
 
+  def expence
+    @bookings = Booking.where(:date => self.start_date..self.end_date)
+    expence = 0.0
+    @bookings.each do |booking|
+      salary = booking.employee.salaries.on_month(booking.date).order("salaries.year_month DESC").first
+      salary_type = salary.sal_type
+      if salary_type == 1
+        date = booking.date
+        date = Date.civil(date.year, date.month, 1)
+        elem_cost = booking.hours.to_f / (MonthWorkingDay.on_month(date).first.working_days) * salary.amount  #???????????????
+      elsif salary_type == 2
+        elem_cost = booking.hours * salary.amount
+      elsif salary_type == 3
+        elem_cost = 0
+      end
+      expence += elem_cost
+    end
+    expence
+  end
+
 end
+
