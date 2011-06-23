0e24b8347516d3c6056ac63d948443b11bce9c9f
Alexey Sukhoviy <martinsilenn@gmail.com>
2011-06-23 14:47:57 +0300

    Validations for Working days

--- app/models/month_working_day.rb
+++ app/models/month_working_day.rb
@@ -2,6 +2,8 @@ class MonthWorkingDay < ActiveRecord::Base
 
   scope :on_month, lambda { |date| { :conditions => ["month_working_days.year_month = ?", date] } }
 
+  validates_numericality_of :working_days, :greater_than => 0
+
   def self.find_by_object(mwday)
     date_sql = Date.civil(mwday.year_month.year,mwday.year_month.month,mwday.year_month.day)
     finded_mwday = self.where(:year_month => date_sql).first
